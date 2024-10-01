b7581f79a5af09ea2b4d9a6c58946afb2641a9ee
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2024-10-01 15:42:29 +0530

    change  export

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
+export default nextConfig;
 
 
 //---------------------------
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = '';
-let basePath = '';
+// let assetPrefix = '';
+// let basePath = '';
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
-module.exports = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support dynamic image optimization
-  },
-  trailingSlash: true, // Ensures that all paths end with a '/'
-};
+// module.exports = {
+//   output: 'export',
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   images: {
+//     unoptimized: true, // GitHub Pages doesn't support dynamic image optimization
+//   },
+//   trailingSlash: true, // Ensures that all paths end with a '/'
+// };
