cad01b4aaad2913ab392b0cb89528917ed6f61a0
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2024-10-01 16:52:23 +0530

    update build with deploy out folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //  output: 'export',
+  output: 'export',
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
   // pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   // extension: /\.mdx?$/,
 
