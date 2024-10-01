1b6a91a500d8d3031d2dda8707f22caed22b0192
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2024-10-01 18:28:21 +0530

    update url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://rohitjbcodeapp.github.io/',
+    siteUrl: 'https://rohitjbcodeapp.github.io/nextcicd',
     generateRobotsTxt: true,
   };
   
