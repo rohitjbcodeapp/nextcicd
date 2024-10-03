75eac160dc78fda5703c0ccaaecfc2ab28f710d2
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2024-10-03 15:30:36 +0530

    add design text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <>
+            <div className="text-8xl text-center font-two fw-9 fs-50 text-primary mb-4 trns-text bg-primary text-uppercase lh-65">
+                Social Media Management Platform
+            </div>
+        </>
+    )
 }
 
 export default About
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hello, World!!</h1>
-        <Link href="/posts" className="text-2xl">Blogs</Link>
+        <h1 className="text-8xl  font-two fw-9 fs-50 text-primary mb-4 trns-text bg-primary text-uppercase lh-65">Hello, World!!</h1>
+        <Link href="/posts" className="text-2xl text-center">Blogs</Link>
         {/* <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
